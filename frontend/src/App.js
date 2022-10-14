74771354eaded82b259668421000fbcc7795ad24
davquipe <davidcondori99@gmail.com>
2022-10-13 22:09:06 -0500

    [ADD] Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href="/">tech</a>
+      </header>
+      <main>lista de productos</main>
+    </div>
+  );
+}
+
+export default App;
