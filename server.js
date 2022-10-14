4ae7e5ef23220ef08ec459010941bf731be6f24f
davquipe <davidcondori99@gmail.com>
2022-10-14 11:10:19 -0500

    [ADD] Basa de Productos

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ import express from 'express';
 import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('connected to db');
+    console.log('conectado a la base de datos');
   })
   .catch((err) => {
     console.log(err.message);
@@ -16,9 +18,8 @@ mongoose
 
 const app = express();
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
