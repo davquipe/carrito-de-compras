41124aabec869d46c77ca20dbcb0c8909081f9f1
davquipe <davidcondori99@gmail.com>
2022-10-13 22:29:08 -0500

    [ADD] Listado de Productos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header className="App-header">
         <a href="/">tech</a>
       </header>
-      <main>lista de productos</main>
+      <main>
+        <h1>Productos Destacados</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>S/{product.price}</strong>
+                </p>
+                <button>Agregar a carrito</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
