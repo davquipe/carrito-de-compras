ef97d683c24381a0135ae96baa63545cdd8bca3c
davquipe <davidcondori99@gmail.com>
2022-10-13 21:24:44 -0500

    [ADD] Create NodeJS Server

---
+++ data.js
@@ -0,0 +1,114 @@
+const data = {
+  products: [
+    {
+      name: 'Mac Book Air',
+      slug: 'mac-book-air',
+      category: 'Mac',
+      image:
+        'https://res.cloudinary.com/dxm7e3hhl/image/upload/v1665712072/p10_snqdt6.jpg',
+      price: 4100,
+      countInStock: 10,
+      brand: 'Apple',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'Diseño Menos peso. Más de todo. Revolucionado con el nuevo chip M2, el MacBook Air cautiva no solo por el diseño ultrafino sino por la rapidez y eficiencia que esconde su resistente carcasa de aluminio. Con un ordenador tan versátil podrás trabajar, jugar y liberar toda tu creatividad vayas donde vayas.',
+    },
+    {
+      name: 'Mac Book Pro',
+      slug: 'mac-book-pro',
+      category: 'Mac',
+      image:
+        'https://res.cloudinary.com/dxm7e3hhl/image/upload/v1665711902/p3_buc7zv.jpg',
+      price: 8100,
+      countInStock: 10,
+      brand: 'Apple',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'Diseño Menos peso. Más de todo. Revolucionado con el nuevo chip M2, el MacBook Pro cautiva no solo por el diseño ultrafino sino por la rapidez y eficiencia que esconde su resistente carcasa de aluminio. Con un ordenador tan versátil podrás trabajar, jugar y liberar toda tu creatividad vayas donde vayas.',
+    },
+    {
+      name: 'iPhone 11',
+      slug: 'iphone-11',
+      category: 'Smartphone',
+      image:
+        'https://res.cloudinary.com/dxm7e3hhl/image/upload/v1665711902/p8_xlziil.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Apple',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      name: 'iPhone 12',
+      slug: 'iphone-12',
+      category: 'Smartphone',
+      image:
+        'https://res.cloudinary.com/dxm7e3hhl/image/upload/v1665711902/p6_v9sxlf.jpg',
+      price: 25,
+      countInStock: 15,
+      brand: 'Apple',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+    {
+      name: 'AirPods Max',
+      slug: 'airpods-max',
+      category: 'AirPods',
+      image:
+        'https://res.cloudinary.com/dxm7e3hhl/image/upload/v1665711902/p2_ttlsdz.jpg',
+      price: 299,
+      countInStock: 100,
+      brand: 'Apple',
+      rating: 5,
+      numReviews: 112,
+      description: 'high quality product',
+    },
+    {
+      name: 'iPhone 13',
+      slug: 'iphone-13',
+      category: 'Smartphone',
+      image:
+        'https://res.cloudinary.com/dxm7e3hhl/image/upload/v1665711902/p7_mhvsb3.jpg',
+      price: 499,
+      countInStock: 90,
+      brand: 'Apple',
+      rating: 5,
+      numReviews: 30,
+      description:
+        'Detección de Accidentes pide ayuda por ti cuando tú no puedes',
+    },
+    {
+      name: 'iPhone SE 2022',
+      slug: 'iphone-se-2022',
+      category: 'Smartphone',
+      image:
+        'https://res.cloudinary.com/dxm7e3hhl/image/upload/v1665711902/p5_fcx0xj.jpg',
+      price: 299,
+      countInStock: 10,
+      brand: 'Apple',
+      rating: 3.5,
+      numReviews: 30,
+      description:
+        'Wolframio 100 % reciclado en el Taptic Engine, que supone el 100 % del wolframio del dispositivo',
+    },
+    {
+      name: 'AirPods Pro',
+      slug: 'airpods-pro',
+      category: 'AirPods',
+      image:
+        'https://res.cloudinary.com/dxm7e3hhl/image/upload/v1665711902/p1_jadxfs.jpg',
+      price: 599,
+      countInStock: 30,
+      brand: 'Apple',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'Los AirPods y el estuche de carga son resistentes al sudor y al agua (IPX4)',
+    },
+  ],
+};
+export default data;
